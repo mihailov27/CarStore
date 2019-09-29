fd35f3aec7f3f87d34ad40727a4450483561e3c6
Mihail Mihaylov <msmihaylov89@gmail.com>
2019-09-29 12:57:13 +0300

    Added a maven plugin for EAR container.
    Signed-off-by: Mihail Mihaylov <msmihaylov89@gmail.com>

--- Backend/src/main/java/com/mmihaylov/backend/dao/impl/CarDaoImpl.java
+++ Backend/src/main/java/com/mmihaylov/backend/dao/impl/CarDaoImpl.java
@@ -1,13 +1,14 @@
 package com.mmihaylov.backend.dao.impl;
 
 import com.mmihaylov.backend.dao.core.BaseJpaDao;
+import com.mmihaylov.backend.dao.core.CarDao;
 import com.mmihaylov.backend.dao.core.JpaDao;
 import com.mmihaylov.model.db.Car;
 
 import javax.ejb.Stateless;
 
 @Stateless(name = "carDao")
-public class CarDaoImpl extends BaseJpaDao<Car, Long> implements JpaDao<Car, Long> {
+public class CarDaoImpl extends BaseJpaDao<Car, Long> implements CarDao {
 
 	@Override
 	public Class<Car> getClazz() {
